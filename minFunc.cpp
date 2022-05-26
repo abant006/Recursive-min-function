4e5b4221c1919ab8abc44dd7f545ad7823b260cb
Aaron Bantay <aaronbantay@gmail.com>
2022-05-25 22:14:28 -0700

    Semi-Working recursive function, few test cases that needs passing

--- minFunc.cpp
+++ minFunc.cpp
@@ -2,5 +2,30 @@
 
 
 const int * min(const int arr[], int arrSize) {
+    // tempVal int variable that stores the value in the last index of the array as you go
+    int tempVal = arr[arrSize - 1];
+    // tempPtr that stores the address of that smallest integer as you go
+    const int *tempPtr = nullptr;
 
+    // base case: if size passed in is zero, return nullptr
+    if (arrSize == 0) {
+        return nullptr;
+    } 
+
+    // base case: if there is only one element left in the array, return smalled value
+    else if (arrSize == 1) {
+        // point tempPtr to the LOCATION of the "last" value in the array
+        tempPtr = &tempVal;
+    }
+
+    // recursive case: ...
+    else {
+        tempPtr = min(arr, arrSize - 1);
+        if (tempVal <= *tempPtr)
+        {
+            tempPtr = &tempVal;
+        }
+    }
+
+    return tempPtr;
 }
